402837476c7d81fad45602995320c995d970b15f
sulanipaboda <sulanipaboda98@gmail.com>
2024-01-15 21:07:51 +0530

    update

---
+++ restaurant/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <h1 className='bg-orange'>Hello</h1>
+    </>
+  )
+}
+
+export default App
---
+++ restaurant/src/layout/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+      <nav>Navbar</nav>
+      <footer>Footer</footer>
+    </div>
+  )
+}
+
+export default Main
